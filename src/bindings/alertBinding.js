6987b6eb8155b714ca7016551e120da6eed2d732
Artem Stepanyuk <faulknercs@yandex.ru>
2013-09-12 20:07:21 +0300

    Added alert and progress bindings

---
+++ src/bindings/alertBinding.js
@@ -0,0 +1,28 @@
+﻿ko.bindingHandlers.alert = {
+    init: function (element, valueAccessor) {
+        var $element = $(element),
+            value = valueAccessor(),
+            priority = 'alert-' + ko.unwrap(value.priority);
+
+        $element.addClass('alert alert-block fade in ' + priority);
+
+        return { controlsDescendantBindings: true };
+    },
+
+    update: function (element, valueAccessor) {
+        var value = ko.utils.unwrapProperties(valueAccessor()),
+            usedTemplateEngine = !value.name ? ko.stringTemplateEngine.instance : null,
+            name = value.name || 'alert',
+            data = value.data || {
+               message: value.message, close: function () {
+                   valueAccessor().visible(false);
+               }
+            };
+        
+        if (ko.unwrap(value.visible)) {
+            ko.renderTemplate(name, data, ko.utils.extend({ templateEngine: usedTemplateEngine }, value.templateOptions), element);
+        } else {
+            ko.virtualElements.emptyNode(element);
+        }
+    }
+};
---
+++ src/bindings/progressBinding.js
@@ -0,0 +1,16 @@
+﻿ko.bindingHandlers.progress = {
+    init: function (element, valueAccessor) {
+        var $element = $(element),
+            value = valueAccessor();
+
+        var barWidth = ko.computed(function () {
+            return ko.unwrap(value) + '%';
+        });
+
+        ko.renderTemplate('progress', { value: value, barWidth: barWidth }, { templateEngine: ko.stringTemplateEngine.instance }, element);
+
+        $element.addClass('progress');
+
+        return { controlsDescendantBindings: true };
+    },
+};
