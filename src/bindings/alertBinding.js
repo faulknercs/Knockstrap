88b1ec888b6e31be4250a018eacf50234f427262
Artem Stepanyuk <faulknercs@yandex.ru>
2013-09-24 19:39:24 +0300

    Updated alert binding: added support of observable template name, added support of virtual elements, updated default template

--- src/bindings/alertBinding.js
+++ src/bindings/alertBinding.js
@@ -1,28 +1,27 @@
 ﻿ko.bindingHandlers.alert = {
-    init: function (element, valueAccessor) {
-        var $element = $(element),
-            value = valueAccessor(),
-            priority = 'alert-' + ko.unwrap(value.priority);
-
-        $element.addClass('alert alert-block fade in ' + priority);
-
+    init: function () {
         return { controlsDescendantBindings: true };
     },
 
     update: function (element, valueAccessor) {
-        var value = ko.utils.unwrapProperties(valueAccessor()),
+        var value = valueAccessor(),
             usedTemplateEngine = !value.name ? ko.stringTemplateEngine.instance : null,
-            name = value.name || 'alert',
+            name = ko.unwrap(value.name) || 'alert',
+
             data = value.data || {
-               message: value.message, close: function () {
-                   valueAccessor().visible(false);
-               }
+                message: value.message,
+                priority: ko.computed(function() {
+                    return 'alert-' + ko.unwrap(value.priority);
+                }),
+                
+                close: value.close || function () {
+                    ko.virtualElements.emptyNode(element);
+                }
             };
-        
-        if (ko.unwrap(value.visible)) {
-            ko.renderTemplate(name, data, ko.utils.extend({ templateEngine: usedTemplateEngine }, value.templateOptions), element);
-        } else {
-            ko.virtualElements.emptyNode(element);
-        }
+
+
+        ko.renderTemplate(name, data, ko.utils.extend({ templateEngine: usedTemplateEngine }, value.templateOptions), element);
     }
 };
+
+ko.virtualElements.allowedBindings.alert = true;
