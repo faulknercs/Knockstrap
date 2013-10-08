000ca3e9ea868bfcad3c0180819cded516212f46
Artem Stepanyuk <faulknercs@yandex.ru>
2013-10-08 20:21:10 +0300

    Added toggle binding

---
+++ src/bindings/toggleBinding.js
@@ -0,0 +1,14 @@
+﻿ko.bindingHandlers.toggle = {
+    init: function (element, valueAccessor) {
+        var value = valueAccessor();
+
+        ko.utils.registerEventHandler(element, 'click', function() {
+            var previousValue = ko.unwrap(value);
+            value(!previousValue);
+        });
+    },
+    
+    update: function (element, valueAccessor) {
+        ko.utils.toggleDomNodeCssClass(element, 'active', ko.unwrap(valueAccessor()));
+    }
+};
