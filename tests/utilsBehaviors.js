e6fb96ffb92515f869138d6df4fe4579fa845fa2
Artem Stepanyuk <faulknercs@yandex.ru>
2014-03-28 01:40:10 +0300

    Begin testing branch

---
+++ tests/utilsBehaviors.js
@@ -0,0 +1,9 @@
+﻿describe('uniqueId', function () {
+    it('Should return different strings for each call', function () {
+        expect(ko.utils.uniqueId()).not.toEqual(ko.utils.uniqueId());
+    });
+
+    it('Should return id starts with passed prefix', function() {
+        expect(ko.utils.uniqueId('test-prefix')).toMatch(/^test-prefix/);
+    });
+});
