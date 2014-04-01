ae5d6624cbe4234a7df7d78a3c3aaf1fc971226f
Artem Stepanyuk <faulknercs@yandex.ru>
2014-04-02 01:08:04 +0300

    Added jasmineExtension, updated progress binding tests

--- tests/bindings/progressBindingBehavior.js
+++ tests/bindings/progressBindingBehavior.js
@@ -1,3 +1,10 @@
 ﻿describe('Binding: progress', function() {
+    this.prepareTestElement();
 
+    it('Should add "progress" class to element', function() {
+        this.testElement.append('<div data-bind="progress: val"></div>');
+        ko.applyBindings({ val: 1 }, this.testElement[0]);
+
+        expect(this.testElement.children().eq(0).hasClass('progress')).toBe(true);
+    });
 });
---
+++ tests/jasmineExtensions.js
@@ -0,0 +1,9 @@
+﻿jasmine.Suite.prototype.prepareTestElement = function () {
+    beforeEach(function() {
+        this.testElement = $("<div></div>");
+    });
+
+    afterEach(function () {
+        this.testElement.remove();
+    });
+};
