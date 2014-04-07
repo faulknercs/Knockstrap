5c2d8eefde1628cd1607cca26171d924316b2f29
Artem Stepanyuk <faulknercs@yandex.ru>
2014-04-07 17:06:52 +0300

    Added radio binding tests, strated checkbox binding tests

--- tests/bindings/checkboxBindingBehaviors.js
+++ tests/bindings/checkboxBindingBehaviors.js
@@ -1,3 +1,14 @@
 ﻿describe('Binding: checkbox', function () {
+    this.prepareTestElement('<div class="btn-group form-group" data-toggle="buttons" data-bind="checkbox: value">'
+        + '<label class="btn btn-primary"><input type="checkbox" value="A" />A</label>'
+        + '<label class="btn btn-primary"><input type="checkbox" value="B" />B</label>'
+        + '</div>');
 
+    it('Should throw exception for non-observable value', function () {
+        var el = this.testElement[0];
+
+        expect(function () {
+            ko.applyBindings({ value: "A" }, el);
+        }).toThrow();
+    });
 });
