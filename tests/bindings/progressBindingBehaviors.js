542b21c84ac4598dd5864776c7e7537b61ddf981
Artem Stepanyuk <faulknercs@yandex.ru>
2014-04-11 13:50:00 +0300

    Updated progress binding tests for usage of jasmine-jquery

--- tests/bindings/progressBindingBehaviors.js
+++ tests/bindings/progressBindingBehaviors.js
@@ -4,14 +4,19 @@
     it('Should add "progress" class to target element', function() {
         ko.applyBindings({ value: 1 }, this.testElement[0]);
 
-        expect(this.testElement.hasClass('progress')).toBe(true);
+        expect(this.testElement).toHaveClass('progress');
     });
 
-    it('Should add width in percents and aria-valuenow attr according to value to progress-bar', function() {
-        ko.applyBindings({ value: 20 }, this.testElement[0]);
+    it('Should add width in percents and aria-valuenow attr according to value to progress-bar', function () {
         
-        expect(parseInt(this.testElement.find('.progress-bar')[0].style.width)).toEqual(20);
-        expect(+this.testElement.find('.progress-bar').attr('aria-valuenow')).toEqual(20);
+        ko.applyBindings({ value: 20 }, this.testElement[0]);
+
+        // need, because if element is visible, jQuery calculate its width in pixels
+        // see http://stackoverflow.com/a/19873744 for details
+        this.testElement.hide();
+        
+        expect(this.testElement.find('.progress-bar')).toHaveCss({ width: '20%' });
+        expect(this.testElement.find('.progress-bar')).toHaveAttr('aria-valuenow', '20');
     });
 
     it('Should change width and aria-valuenow attr according to value changes', function () {
@@ -21,10 +26,11 @@
 
         ko.applyBindings(vm, this.testElement[0]);
 
+        this.testElement.hide();
         vm.value(30);
 
-        expect(parseInt(this.testElement.find('.progress-bar')[0].style.width)).toEqual(30);
-        expect(+this.testElement.find('.progress-bar').attr('aria-valuenow')).toEqual(30);
+        expect(this.testElement.find('.progress-bar')).toHaveCss({ width: '30%' });
+        expect(this.testElement.find('.progress-bar')).toHaveAttr('aria-valuenow', '30');
     });
 
     it('Should throw exception for not Number value', function () {
