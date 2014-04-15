1cf15c5b3d9d20654453aa1c12af394bab134a8e
Artem Stepanyuk <faulknercs@yandex.ru>
2014-04-15 17:42:54 +0300

    Added tests for carousel bindings

--- tests/bindings/carouselBindingBehaviors.js
+++ tests/bindings/carouselBindingBehaviors.js
@@ -1,3 +1,34 @@
 ﻿describe('Binding: carousel', function () {
+    this.prepareTestElement('<div data-bind="carousel: value"></div>');
 
+    var testData = [{
+        src: 'testsrc-one',
+        alt: 'First image',
+        content: 'First caption'
+    }, {
+        src: 'testsrc-two',
+        alt: 'Second image',
+        content: 'Second caption'
+    }];
+
+    it('Should throw exception, if "content" is not passed', function() {
+        var el = this.testElement[0];
+
+        expect(function() {
+            ko.applyBindings({}, el);
+        }).toThrow();
+    });
+    
+    it('Should create carousel with passed id', function () {
+        var vm = {
+            value: {
+                id: 'test-id',
+                content: { data: testData }
+            }
+        };
+
+        ko.applyBindings(vm, this.testElement[0]);
+
+        expect(this.testElement).toHaveId('test-id');
+    });
 });
