a6006794e92cbc44b6e46e062863bce5cbc48697
Artem Stepanyuk <faulknercs@yandex.ru>
2014-04-19 12:59:08 +0300

    Fixed radio binding to make tests green: added exception throwing for non-observables and unchecking of buttons, when there is now passed value

--- src/bindings/radioBinding.js
+++ src/bindings/radioBinding.js
@@ -3,6 +3,10 @@ ko.bindingHandlers.radio = {
     init: function (element, valueAccessor) {
         var $element = $(element);
 
+        if (!ko.isObservable(valueAccessor())) {
+            throw new Error('radio binding should be used only with observable values');
+        }
+
         $element.on('change', 'input:radio', function (e) {
             // we need to handle change event after bootsrap will handle its event
             // to prevent incorrect changing of radio button styles
@@ -18,13 +22,19 @@ ko.bindingHandlers.radio = {
 
     update: function (element, valueAccessor) {
         var radioButton = $(element).find('input[value="' + ko.unwrap(valueAccessor()) + '"]'),
-            radioButtonWrapper = radioButton.parent();
+            radioButtonWrapper;
 
         if (radioButton.length) {
+            radioButtonWrapper = radioButton.parent();
+
             radioButtonWrapper.siblings().removeClass('active');
             radioButtonWrapper.addClass('active');
 
-            radioButton[0].checked = true;
+            radioButton.prop('checked', true);
+        } else {
+            radioButtonWrapper = $(element).find('.active');
+            radioButtonWrapper.removeClass('active');
+            radioButtonWrapper.find('input').prop('checked', false);
         }
     }
 };
