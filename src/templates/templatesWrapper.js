4a13f4f78ddf1903a3d6fd89a887250bf4d9a945
Artem Stepanyuk <faulknercs@yandex.ru>
2014-05-07 17:26:37 +0300

    Rewrote string template engine implementation

--- src/templates/templatesWrapper.js
+++ src/templates/templatesWrapper.js
@@ -1,10 +1,10 @@
 ﻿// inspired by http://www.knockmeout.net/2011/10/ko-13-preview-part-3-template-sources.html
-ko.stringTemplateEngine = function () {
-    var templates = {},
-        templateEngine = new ko.nativeTemplateEngine();
+(function() {
+    var templates = {};
 
     // @include compiledTemplates.js
-    
+
+    // create new template source to provide storing templates in strings
     ko.templateSources.stringTemplate = function (template) {
         this.templateName = template;
 
@@ -28,15 +28,17 @@ ko.stringTemplateEngine = function () {
         };
     };
 
-    templateEngine.makeTemplateSource = function (template) {
+    // create modified template engine, which uses new string template source
+    ko.stringTemplateEngine = function () {
+        this.allowTemplateRewriting = false;
+    };
+
+    ko.stringTemplateEngine.prototype = new ko.nativeTemplateEngine();
+    ko.stringTemplateEngine.prototype.constructor = ko.stringTemplateEngine;
+    
+    ko.stringTemplateEngine.prototype.makeTemplateSource = function (template) {
         return new ko.templateSources.stringTemplate(template);
     };
-
-    templateEngine.addTemplate = function(name, body) {
-        templates[name] = body;
-    };
-
-    return templateEngine;
-};
+})();
 
 ko.stringTemplateEngine.instance = new ko.stringTemplateEngine();
