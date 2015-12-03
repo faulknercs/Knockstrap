394cdd8e385d94c9a3d2369363252f0cc909c5e0
Artem Stepanyuk <faulknercs@yandex.ru>
2015-12-03 13:20:45 +0300

    Removed unwrapProperties implementation as ko.toJS do the same. ko.utils.unwrapProperties is alias for ko.toJS now for compatibility.

--- src/utils/unwrapProperties.js
+++ src/utils/unwrapProperties.js
@@ -1,14 +1,1 @@
-﻿ko.utils.unwrapProperties = function (wrappedProperies) {
-
-    if (wrappedProperies === null || typeof wrappedProperies !== 'object') {
-        return wrappedProperies;
-    }
-
-    var options = {};
-
-    ko.utils.objectForEach(wrappedProperies, function (propertyName, propertyValue) {
-        options[propertyName] = ko.unwrap(propertyValue);
-    });
-
-    return options;
-};
+﻿ko.utils.unwrapProperties = ko.toJS;
